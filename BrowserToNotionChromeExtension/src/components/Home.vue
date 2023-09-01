63ed627647318e07a103c2c097dacbb0afc01fbc
Gerald <geraldson.a.P@gmail.com>
2023-09-01 10:54:37 -0400

    Adds permision to manifest and funtion to get current page url

--- BrowserToNotionChromeExtension/src/components/Home.vue
+++ BrowserToNotionChromeExtension/src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <h2>Sending to: {{ databaseInfo?.title.text.content}}</h2>
 
-  <div class="">
-    <button class="px-2 py-1 text-gray-50 bg-black rounded-md hover:outline-1 outline-stone-500">Send to Notion</button>
+  <div class="flex justify-center mt-8">
+    <button @click="getTabs" class="px-2 py-1 text-gray-50 bg-black rounded-md hover:outline-1 outline-stone-500">Send to Notion</button>
   </div>
 </template>
 
@@ -11,6 +11,16 @@ import { onMounted, ref } from "vue";
 import api from "../services/api";
 
 const databaseInfo = ref(null)
+
+const getTabs = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+    const url = activeTab.url;
+    const title = activeTab.title;
+    const favIconUrl = activeTab.favIconUrl;
+    console.log(activeTab);
+  });
+};
 
 onMounted( () => {
   api.getDatabaseInfo().then( (res) => {
